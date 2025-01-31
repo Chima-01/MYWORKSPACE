25bd9eedd40a191c861b929b04d4e1afb4edb4f2
Chinemerem <divinechima76@gmail.com>
2025-01-31 20:20:11 +0100

    first hng12 commit

---
+++ hng12_backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000 
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const date = new Date();
+
+  res.status(200).json({
+    email: 'divinechima76@gmail.com',
+    current_datetime: date.toDateString(),
+    github_url: "https://github.com/Chima-01/MYWORKSPACE/hng12_backend"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
