7c5f99af2bad14bee4901ce5563fc11326963722
Chinemerem <divinechima76@gmail.com>
2025-09-23 17:22:33 +0100

    fix(auth): escape single quote in login page to resolve react/no-unescaped-entities ESLint error

--- NEXTJS/alx-poll/app/(auth)/login/page.tsx
+++ NEXTJS/alx-poll/app/(auth)/login/page.tsx
@@ -114,7 +114,7 @@ export default function LoginPage() {
         </div>
 
         <p className="mt-6 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/register" className="font-semibold text-blue-600 hover:text-blue-500 transition-colors">
             Sign Up
           </Link>
